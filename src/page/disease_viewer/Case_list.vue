94dc4d5a7dc96d6f352761b6b39e40fb58eeb0fd
niyuancheng <nyc20010820@126.com>
2023-03-29 19:24:26 +0800

    完成用户token验证的逻辑

--- src/page/disease_viewer/Case_list.vue
+++ src/page/disease_viewer/Case_list.vue
@@ -2,25 +2,39 @@
     <div id="case_list">
         <div id="header">
             <h1>病例总览</h1>
+            
         </div>
-        <Disease_list />
-        <Disease_selector />
+        <div class="case_list-container" v-if="$store.state.status === 1">
+            <Disease_list />
+            <Disease_selector />
+        </div>
+        <div v-else-if="$store.state.status === 0">
+            <el-empty description="您暂未登录，无法查看病例信息"></el-empty>
+        </div>
     </div>
 </template>
 
 <script>
 import Disease_list from '@/component/Disease_list.vue';
 import Disease_selector from '@/component/Disease_selector.vue';
+import { NetLoader } from '@/net';
 export default {
     name: "Case_list",
     data() {
         return {
-            
+            loader: new NetLoader("test")
         };
     },
     methods: {
     },
-    components: { Disease_list,Disease_selector }
+    created() {
+        this.loader.get("/user/verify").then(value => {
+            this.$store.commit("changeStatus",1);
+        }, err => {
+            this.$store.commit("changeStatus",0);
+        })
+    },
+    components: { Disease_list,Disease_selector },
 }
 </script>
 
