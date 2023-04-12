86fb2ed703083011d27f43d41b5b6bf193ba6ffc
Treven Tang <rqtangchn@gmail.com>
2023-04-12 15:30:34 +0800

    add exam timer

--- src/page/test/Exams.vue
+++ src/page/test/Exams.vue
@@ -1,8 +1,28 @@
 <template>
     <div>
-        <el-menu class="exam-nav">
+        <el-table :data="exams" border style="width: 100%;margin-top: 20px;"
+        :header-cell-style="{background: 'rgb(242, 243, 244)',color:'#515a6e'}">
+            <el-table-column prop="id" label="考试名称">
+                <template slot-scope="scope">
+                    考试{{ scope.row.id }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="testpaper.period" label="考试时间">
+            </el-table-column>
+            <el-table-column prop="testpaper.total_score" label="总分">
+            </el-table-column>
+            <el-table-column fixed="right" label="操作">
+                <template slot-scope="scope">
+                    <el-button type="primary" size="mini" @click="show_paper(scope.row.testpaper)">参加考试</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+
+        <!-- <el-menu class="exam-nav">
             <el-menu-item :key="item.id" v-for="item in exams" v-on:click="show_paper(item.testpaper)">考试{{ item.id }}</el-menu-item>
-        </el-menu>
+        </el-menu> -->
+        
+        
     </div>
 </template>
 
@@ -18,8 +38,11 @@ export default {
     methods: {
         show_paper: function(testpaper) {
             this.$router.push({
-                path: '/home/testPaper', query: { questions: testpaper.questions }
+                path: '/home/testPaper', query: { questions: testpaper.questions, period: testpaper.period }
             })
+        },
+        joinExamination: function(row) {
+
         }
     },
     created() {
